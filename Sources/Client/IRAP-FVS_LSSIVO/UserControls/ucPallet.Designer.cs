8c7d27a152f4d8126b8da0a94ea19fdcbfbf7d31
李智颖 <zhiying.li@chinairap.com>
2019-03-21 11:40:56 +0800

    新增 IRAP-FVS_LSSIVO 项目中的 ucPallet 面板

---
+++ Sources/Client/IRAP-FVS_LSSIVO/UserControls/ucPallet.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace IRAP_FVS_LSSIVO.UserControls
+{
+    partial class ucPallet
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.SecondaryAxisY secondaryAxisY1 = new DevExpress.XtraCharts.SecondaryAxisY();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SeriesPoint seriesPoint1 = new DevExpress.XtraCharts.SeriesPoint("背光颜色NG", new object[] {
+            ((object)(14D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint2 = new DevExpress.XtraCharts.SeriesPoint("阅读灯不亮", new object[] {
+            ((object)(13D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint3 = new DevExpress.XtraCharts.SeriesPoint("卡子变形", new object[] {
+            ((object)(12D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint4 = new DevExpress.XtraCharts.SeriesPoint("UDGO回弹不良", new object[] {
+            ((object)(11D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint5 = new DevExpress.XtraCharts.SeriesPoint("产品变形", new object[] {
+            ((object)(10D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint6 = new DevExpress.XtraCharts.SeriesPoint("少料", new object[] {
+            ((object)(9D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint7 = new DevExpress.XtraCharts.SeriesPoint("外观问题", new object[] {
+            ((object)(8D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint8 = new DevExpress.XtraCharts.SeriesPoint("掉漆", new object[] {
+            ((object)(7D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint9 = new DevExpress.XtraCharts.SeriesPoint("排线过长", new object[] {
+            ((object)(6D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint10 = new DevExpress.XtraCharts.SeriesPoint("毛边", new object[] {
+            ((object)(5D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint11 = new DevExpress.XtraCharts.SeriesPoint("异音", new object[] {
+            ((object)(4D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint12 = new DevExpress.XtraCharts.SeriesPoint("缩水", new object[] {
+            ((object)(3D))});
+            DevExpress.XtraCharts.Series series2 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.PointSeriesLabel pointSeriesLabel1 = new DevExpress.XtraCharts.PointSeriesLabel();
+            DevExpress.XtraCharts.SeriesPoint seriesPoint13 = new DevExpress.XtraCharts.SeriesPoint("背光颜色NG", new object[] {
+            ((object)(14D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint14 = new DevExpress.XtraCharts.SeriesPoint("阅读灯不亮", new object[] {
+            ((object)(27D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint15 = new DevExpress.XtraCharts.SeriesPoint("卡子变形", new object[] {
+            ((object)(39D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint16 = new DevExpress.XtraCharts.SeriesPoint("UDGO回弹不良", new object[] {
+            ((object)(50D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint17 = new DevExpress.XtraCharts.SeriesPoint("产品变形", new object[] {
+            ((object)(60D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint18 = new DevExpress.XtraCharts.SeriesPoint("少料", new object[] {
+            ((object)(69D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint19 = new DevExpress.XtraCharts.SeriesPoint("外观问题", new object[] {
+            ((object)(77D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint20 = new DevExpress.XtraCharts.SeriesPoint("掉漆", new object[] {
+            ((object)(84D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint21 = new DevExpress.XtraCharts.SeriesPoint("排线过长", new object[] {
+            ((object)(90D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint22 = new DevExpress.XtraCharts.SeriesPoint("毛边", new object[] {
+            ((object)(95D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint23 = new DevExpress.XtraCharts.SeriesPoint("异音", new object[] {
+            ((object)(99D))});
+            DevExpress.XtraCharts.SeriesPoint seriesPoint24 = new DevExpress.XtraCharts.SeriesPoint("缩水", new object[] {
+            ((object)(102D))});
+            DevExpress.XtraCharts.LineSeriesView lineSeriesView1 = new DevExpress.XtraCharts.LineSeriesView();
+            DevExpress.XtraCharts.ChartTitle chartTitle1 = new DevExpress.XtraCharts.ChartTitle();
+            this.chartPallet = new DevExpress.XtraCharts.ChartControl();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPallet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(secondaryAxisY1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(pointSeriesLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartPallet
+            // 
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisX.VisualRange.Auto = false;
+            xyDiagram1.AxisX.VisualRange.AutoSideMargins = false;
+            xyDiagram1.AxisX.VisualRange.MaxValueSerializable = "缩水";
+            xyDiagram1.AxisX.VisualRange.MinValueSerializable = "背光颜色NG";
+            xyDiagram1.AxisX.VisualRange.SideMarginsValue = 0.5D;
+            xyDiagram1.AxisY.Title.Text = "不良个数";
+            xyDiagram1.AxisY.Title.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            secondaryAxisY1.AxisID = 0;
+            secondaryAxisY1.Label.TextPattern = "{V} %";
+            secondaryAxisY1.Name = "累计比率";
+            secondaryAxisY1.NumericScaleOptions.AutoGrid = false;
+            secondaryAxisY1.NumericScaleOptions.GridSpacing = 20D;
+            secondaryAxisY1.Title.Text = "累计比率";
+            secondaryAxisY1.Title.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            secondaryAxisY1.VisibleInPanesSerializable = "-1";
+            secondaryAxisY1.WholeRange.Auto = false;
+            secondaryAxisY1.WholeRange.AutoSideMargins = false;
+            secondaryAxisY1.WholeRange.MaxValueSerializable = "100";
+            secondaryAxisY1.WholeRange.MinValueSerializable = "0";
+            secondaryAxisY1.WholeRange.SideMarginsValue = 0D;
+            xyDiagram1.SecondaryAxesY.AddRange(new DevExpress.XtraCharts.SecondaryAxisY[] {
+            secondaryAxisY1});
+            this.chartPallet.Diagram = xyDiagram1;
+            this.chartPallet.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartPallet.Legend.AlignmentHorizontal = DevExpress.XtraCharts.LegendAlignmentHorizontal.Center;
+            this.chartPallet.Legend.AlignmentVertical = DevExpress.XtraCharts.LegendAlignmentVertical.BottomOutside;
+            this.chartPallet.Legend.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chartPallet.Location = new System.Drawing.Point(10, 10);
+            this.chartPallet.Name = "chartPallet";
+            series1.Name = "Series 1";
+            series1.Points.AddRange(new DevExpress.XtraCharts.SeriesPoint[] {
+            seriesPoint1,
+            seriesPoint2,
+            seriesPoint3,
+            seriesPoint4,
+            seriesPoint5,
+            seriesPoint6,
+            seriesPoint7,
+            seriesPoint8,
+            seriesPoint9,
+            seriesPoint10,
+            seriesPoint11,
+            seriesPoint12});
+            series2.CrosshairLabelPattern = "{V} %";
+            pointSeriesLabel1.TextPattern = "{V} %";
+            series2.Label = pointSeriesLabel1;
+            series2.LabelsVisibility = DevExpress.Utils.DefaultBoolean.True;
+            series2.LegendTextPattern = "{V} %";
+            series2.Name = "Series 2";
+            series2.Points.AddRange(new DevExpress.XtraCharts.SeriesPoint[] {
+            seriesPoint13,
+            seriesPoint14,
+            seriesPoint15,
+            seriesPoint16,
+            seriesPoint17,
+            seriesPoint18,
+            seriesPoint19,
+            seriesPoint20,
+            seriesPoint21,
+            seriesPoint22,
+            seriesPoint23,
+            seriesPoint24});
+            lineSeriesView1.AxisYName = "累计比率";
+            series2.View = lineSeriesView1;
+            this.chartPallet.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1,
+        series2};
+            this.chartPallet.Size = new System.Drawing.Size(439, 290);
+            this.chartPallet.TabIndex = 0;
+            chartTitle1.Text = "质量问题柏拉图";
+            this.chartPallet.Titles.AddRange(new DevExpress.XtraCharts.ChartTitle[] {
+            chartTitle1});
+            // 
+            // ucPallet
+            // 
+            this.Appearance.BackColor = System.Drawing.Color.White;
+            this.Appearance.Options.UseBackColor = true;
+            this.Controls.Add(this.chartPallet);
+            this.Name = "ucPallet";
+            this.Padding = new System.Windows.Forms.Padding(10);
+            ((System.ComponentModel.ISupportInitialize)(secondaryAxisY1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(pointSeriesLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(lineSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartPallet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraCharts.ChartControl chartPallet;
+    }
+}
---
+++ Sources/Client/IRAP-FVS_LSSIVO/UserControls/ucPallet.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Reflection;
+
+using DevExpress.XtraCharts;
+
+using IRAP.Global;
+using IRAP.Entities.MES;
+using IRAP.WCF.Client.Method;
+
+namespace IRAP_FVS_LSSIVO.UserControls
+{
+    public partial class ucPallet : IRAP_FVS_LSSIVO.UserControls.ucCustomFVSKanban
+    {
+        private static string className =
+            MethodBase.GetCurrentMethod().DeclaringType.FullName;
+
+        private List<FailureModeOfPallet> datas = new List<FailureModeOfPallet>();
+
+        public ucPallet()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 创建右侧纵坐标
+        /// </summary>
+        /// <param name="xyDiagram"></param>
+        /// <param name="series"></param>
+        private void CreateSecondaryAxisY(XYDiagram xyDiagram, Series series)
+        {
+            if (xyDiagram == null)
+                return;
+            if (series == null)
+                return;
+
+            SecondaryAxisY newAxis = new SecondaryAxisY(series.Name);
+            newAxis.Title.Text = series.Name;
+            newAxis.Title.Alignment = StringAlignment.Center;
+            newAxis.Title.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            newAxis.Title.Font = new Font("微软雅黑", 10.5f, FontStyle.Regular);
+            newAxis.Title.TextColor = series.View.Color;
+            newAxis.Label.TextColor = series.View.Color;
+            newAxis.Color = series.View.Color;
+            newAxis.NumericScaleOptions.ScaleMode = ScaleMode.Manual;
+            newAxis.NumericScaleOptions.CustomMeasureUnit = 1;
+            newAxis.WholeRange.SetMinMaxValues(0, 100);
+            newAxis.WholeRange.SideMarginsValue = 0;
+            newAxis.VisualRange.SetMinMaxValues(0, 100);
+            newAxis.VisualRange.SideMarginsValue = 0;
+            newAxis.Label.TextPattern = "{V} %";
+
+            xyDiagram.SecondaryAxesY.Add(newAxis);
+            ((LineSeriesView)series.View).AxisY = newAxis;
+        }
+
+        private void GetFailureModesPallet(
+            int communityID,
+            int productLeaf,
+            int workUnitLeaf,
+            string pwoNo,
+            long sysLogID)
+        {
+            string strProcedureName =
+               string.Format(
+                   "{0}.{1}",
+                   className,
+                   MethodBase.GetCurrentMethod().Name);
+            WriteLog.Instance.WriteBeginSplitter(strProcedureName);
+            try
+            {
+                int errCode = 0;
+                string errText = "";
+
+                IRAPMESClient.Instance.ufn_GetPallet_FailureMode(
+                    communityID,
+                    productLeaf,
+                    workUnitLeaf,
+                    pwoNo,
+                    sysLogID,
+                    ref datas,
+                    out errCode,
+                    out errText);
+                WriteLog.Instance.Write(
+                    string.Format("({0}){1}", errCode, errText),
+                    strProcedureName);
+
+                chartPallet.Series.Clear();
+                if (errCode == 0)
+                {
+                    Series series1 = new Series("不良个数", ViewType.Bar)
+                    {
+                        ArgumentScaleType = ScaleType.Qualitative,
+                        LabelsVisibility = DevExpress.Utils.DefaultBoolean.True,
+                    };
+                    Series series2 = new Series("累计比率", ViewType.Line)
+                    {
+                        ArgumentScaleType = ScaleType.Qualitative,
+                        LabelsVisibility = DevExpress.Utils.DefaultBoolean.True,
+                    };
+
+                    foreach (FailureModeOfPallet data in datas)
+                    {
+                        string seriesCaption = string.Format("{0}",
+                            data.T118Name,
+                            data.T118Code);
+                        if (data.FailureQty > 0)
+                        {
+                            series1.Points.Add(new SeriesPoint(seriesCaption, data.FailureQty));
+                        }
+                        if (data.FailRate > 0)
+                        {
+                            series2.Points.Add(
+                                new SeriesPoint(
+                                    seriesCaption, 
+                                    string.Format(
+                                        "{0}",
+                                        data.FailRate * 100)));
+                        }
+                    }
+
+                    //
+                    series1.Label.Border.Visibility = DevExpress.Utils.DefaultBoolean.False;
+                    series2.Label.Border.Visibility = DevExpress.Utils.DefaultBoolean.False;
+                    //
+                    series1.Label.TextColor = Color.Black;
+                    series2.Label.TextColor = Color.Black;
+                    series1.Label.BackColor = System.Drawing.Color.Transparent;
+                    series2.Label.BackColor = System.Drawing.Color.Transparent;
+                    series2.ToolTipHintDataMember = "{V} %";
+                    //
+                    series1.LabelsVisibility = DevExpress.Utils.DefaultBoolean.True;
+                    series2.LabelsVisibility = DevExpress.Utils.DefaultBoolean.False;
+
+                    chartPallet.Series.Add(series1);
+                    chartPallet.Series.Add(series2);
+
+                    XYDiagram xyDiagram = (XYDiagram)chartPallet.Diagram;
+                    if (xyDiagram != null)
+                    {
+                        xyDiagram.SecondaryAxesY.Clear();
+                        xyDiagram.AxisX.VisualRange.Auto = false;
+                        xyDiagram.AxisX.VisualRange.AutoSideMargins = false;
+
+                        xyDiagram.AxisY.Title.Text = "不良个数";
+                        xyDiagram.AxisY.Title.Font = new Font("微软雅黑", 10.5f, FontStyle.Regular);
+                        xyDiagram.AxisY.Title.Visibility = DevExpress.Utils.DefaultBoolean.True;
+
+                        CreateSecondaryAxisY(xyDiagram, series2);
+                    }
+                }
+            }
+            finally
+            {
+                WriteLog.Instance.WriteEndSplitter(strProcedureName);
+            }
+        }
+
+        public void SetSearchCondition(
+            int communityID,
+            int productLeaf,
+            int workUnitLeaf,
+            string pwoNo,
+            long sysLogID)
+        {
+            GetFailureModesPallet(
+                communityID,
+                productLeaf,
+                workUnitLeaf,
+                pwoNo,
+                sysLogID);
+        }
+    }
+}
