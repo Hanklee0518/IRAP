01345177f066c86f81077f1f647ba6c7caed63c7
李智颖 <zhiying.li@chinairap.com>
2019-04-24 12:59:21 +0800

    产品信息跟踪卡打印模板中增加生产工单的条码

--- Sources/Client/GUI/IRAP.Client.GUI.SCES/Properties/AssemblyInfo.cs
+++ Sources/Client/GUI/IRAP.Client.GUI.SCES/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
-[assembly: AssemblyVersion("6.1.32.143")]
+[assembly: AssemblyVersion("6.1.34.145")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("6.1.0326.143")]
-[assembly: AssemblyInformationalVersion("6.1.32.143")]
+[assembly: AssemblyFileVersion("6.1.0424.145")]
+[assembly: AssemblyInformationalVersion("6.1.34.145")]
