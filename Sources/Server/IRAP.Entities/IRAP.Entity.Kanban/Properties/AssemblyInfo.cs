6a6b7db7995c12e87cc15912a461f1a6a555d230
李智颖 <zhiying.li@chinairap.com>
2019-04-25 19:38:37 +0800

    产品信息跟踪卡打印中增加生产工单条码完成最后调试，顺利上线。

--- Sources/Server/IRAP.Entities/IRAP.Entity.Kanban/Properties/AssemblyInfo.cs
+++ Sources/Server/IRAP.Entities/IRAP.Entity.Kanban/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
-[assembly: AssemblyVersion("6.1.167.1172")]
+[assembly: AssemblyVersion("6.1.169.1177")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("6.1.167.1172")]
-[assembly: AssemblyFileVersion("6.1.0425.1172")]
+[assembly: AssemblyInformationalVersion("6.1.169.1177")]
+[assembly: AssemblyFileVersion("6.1.0425.1177")]
