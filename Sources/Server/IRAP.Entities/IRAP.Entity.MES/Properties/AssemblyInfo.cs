18d676053ea6fb101df00a107471148ad63b2a1c
李智颖 <zhiying.li@chinairap.com>
2019-04-25 13:59:57 +0800

    新增万能表单执行中的播放提示声音的Action，同时更新 OutputStr 接口规范。

--- Sources/Server/IRAP.Entities/IRAP.Entity.MES/Properties/AssemblyInfo.cs
+++ Sources/Server/IRAP.Entities/IRAP.Entity.MES/Properties/AssemblyInfo.cs
@@ -31,8 +31,8 @@ using System.Runtime.InteropServices;
 //
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值，
 // 方法是按如下所示使用“*”: :
-[assembly: AssemblyVersion("6.1.253.1260")]
+[assembly: AssemblyVersion("6.1.254.1262")]
 //[assembly: AssemblyVersion("1.0.0.0")]
 //[assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("6.1.253.1260")]
-[assembly: AssemblyFileVersion("6.1.0425.1260")]
+[assembly: AssemblyInformationalVersion("6.1.254.1262")]
+[assembly: AssemblyFileVersion("6.1.0425.1262")]
